4185a249e690cf9a8f370763ad1bbaa69160047a
Burlet Médéric <mederic.burlet@gmail.com>
2017-11-13 09:03:35 +0800

    start design

--- SmiterApp/src/components/home/Home.js
+++ SmiterApp/src/components/home/Home.js
@@ -1,15 +1,68 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
-const Home = () => {
-   const goToAbout = () => {
-      Actions.about()
-   }
-   return (
-      <TouchableOpacity style = {{ margin: 128 }} onPress = {goToAbout}>
+const s_home = require("../../styles/s_home.js");
+
+export const ENTRIES1 = [
+    {
+        mainData: "zest_points",
+        highlightedText: "Zest points won",
+        normalText: "since registration",
+        colorScheme: "#fff"
+    },
+    {
+        mainData: "activated_boosts_count",
+        highlightedText: "Boost",
+        normalText: "activated",
+        colorScheme: "#0065fe"
+    },
+    {
+        mainData: "total_reward",
+        highlightedText: "Rewards",
+        normalText: "redeemed",
+        colorScheme: "#fcbe00"
+    },
+];
+
+function test(){
+  for (i=0; i< ENTRIES1.length;i++){
+    var temp = ENTRIES1[i].mainData
+    console.warn(temp)
+    //return(<Text>{temp}</Text>)
+  }
+}
+
+const goToAbout = () => {
+   Actions.about()
+}
+
+export default class Home extends React.Component {
+
+
+
+   render() {
+
+     contents = ENTRIES1.map(function (item) {
+        return (
+          <View key={item.mainData} style={s_home.test }>
+            <Text>{item.mainData}</Text>
+          </View>
+        );
+     });
+     return (
+      <View>
+        <View>
+        <Text style={{textAlign:"center"}}>XXX</Text>
+        </View>
+        <View>
+            { contents }
+        </View>
+        <TouchableOpacity style = {{ margin: 128 }} onPress = {goToAbout}>
          <Text>This is HOME!</Text>
       </TouchableOpacity>
-   )
+      </View>
+    );
+  }
+
 }
-export default Home
---
+++ SmiterApp/src/styles/s_home.js
@@ -0,0 +1,18 @@
+'use strict';
+var React = require('react-native');
+var {
+  StyleSheet,
+} = React;
+import Dimensions from 'Dimensions';
+const {screenwidth, screenheight} = Dimensions.get('window');
+
+module.exports = StyleSheet.create({
+
+test:{
+  backgroundColor:"#ff0000",
+
+
+}
+
+
+});
